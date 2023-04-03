598e02e3c7a1d6a98295d632520d9eb1dd749bbc
shrumidedhia03 <shrumi.dedhia@gmail.com>
2023-04-03 18:26:01 -0400

    Final Commit

---
+++ config/default.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: "dev",
+  mysql: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "mysql",
+    port: 3306,
+  },
+  sendgridKey: "yourkey",
+};
